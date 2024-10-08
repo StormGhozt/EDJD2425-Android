f456ebcce2a59bce1293334420d229cf39be0925
Vítor Guerra <a27950@alunos.ipca.pt>
2024-10-08 15:55:30 +0100

    fixed opperator function added colors and boxes

--- Calculator/app/src/main/java/com/example/calculator/Calculator.kt
+++ Calculator/app/src/main/java/com/example/calculator/Calculator.kt
@@ -1,29 +1,33 @@
 package com.example.calculator
 
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-
+import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,72 +35,86 @@ import com.example.calculator.ui.theme.CalculatorTheme
 
 @Composable
 fun CalculatorScreen(modifier: Modifier = Modifier) {
+    var textStyle = remember { mutableStateOf(TextStyle(fontSize = 100.sp, color = Color.White)) }
     var display by remember { mutableStateOf("0") }
-    var currentValue by remember { mutableStateOf<Double>(0.0) }
-    var previousValue by remember { mutableStateOf<Double>(0.0) }
-    var operator by remember { mutableStateOf<String?>(null) }
+    var operand by remember { mutableDoubleStateOf(0.0) }
+    var operator by remember { mutableStateOf("") }
+    var userIsEnteringNums by remember { mutableStateOf(true) }
 
     fun buttonNum(buttext: String) {
         when (buttext) {
             "CE" -> {
+                textStyle.value = textStyle.value.copy(fontSize = 100.sp)
                 display = "0"
-                currentValue = 0.0
+                userIsEnteringNums = true
             }
             "C" -> {
+                textStyle.value = textStyle.value.copy(fontSize = 100.sp)
                 display = "0"
-                currentValue = 0.0
-                previousValue = 0.0
-                operator = null
+                operand = 0.0
+                operator = ""
+                userIsEnteringNums = true
             }
             "%" -> {
-                currentValue = display.toDouble() / 100
-                display = currentValue.toString()
+                operand = display.toDouble() / 100
+                display = operand.toString()
+                userIsEnteringNums = false
             }
-            "÷", "x", "-", "+" -> {
-                operator = buttext
-                currentValue = display.toDouble()
-                display = "0" // Reset display for the second number
-                previousValue = currentValue
-                currentValue = 0.0 // Reset currentValue for the second number
-            }
-            "=" -> {
-                if (operator != null && previousValue != 0.0) {
-                    currentValue = display.toDouble()
-                    val result = when (operator) {
-                        "÷" -> previousValue / currentValue
-                        "x" -> previousValue * currentValue
-                        "-" -> previousValue - currentValue
-                        "+" -> previousValue + currentValue
-                        else -> 0.0
+            "÷", "x", "-", "+", "=" -> {
+                if (operator.isNotEmpty()) {
+                    val currentValue = display.toDouble()
+                    when (operator) {
+                        "÷" -> operand /= currentValue
+                        "x" -> operand *= currentValue
+                        "-" -> operand -= currentValue
+                        "+" -> operand += currentValue
                     }
-                    display = result.toString()
-                    previousValue = result
-                    currentValue = result // Update currentValue with the result
-                    operator = null
+                    if (operand % 1 == 0.0) {
+                        display = operand.toInt().toString() // Display result as integer if no fractional part
+                    } else {
+                        display = operand.toString()
+                    }
+                    operator = buttext // Update operator for the next operation
+                }
+                operand = display.toDouble()
+                operator = buttext
+                userIsEnteringNums = false
+                textStyle.value = textStyle.value.copy(fontSize = 100.sp)
+            }
+            "." -> {
+                if (!display.contains(".")) {
+                    display += "."
+                    userIsEnteringNums = true
                 }
             }
             else -> { // Number buttons
-                if (display == "0" ) {
-                    display = buttext // Start a new number after operator
+                if (userIsEnteringNums) {
+                    if (display == "0") {
+                        display = buttext // Start new number if display is 0
+                    } else {
+                        display += buttext // Append to existing number
+                    }
                 } else {
-                    display += buttext // Append to existing number
+                    display = buttext
                 }
-                currentValue = display.toDouble()
+                userIsEnteringNums = true
             }
         }
     }
-
     Box(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally, // Center horizontally
-            verticalArrangement = Arrangement.spacedBy(5.dp),
+            verticalArrangement = Arrangement.spacedBy(0.dp),
             modifier = modifier.fillMaxSize()
         ) {
-            textBox(text = display)
-            Row(modifier = Modifier.fillMaxWidth(), // Fill width for horizontal arrangement
+            Row(modifier=Modifier.weight(2f)){
+                textBox(text = display,textStyle = textStyle)
+
+            }
+            Row(modifier = Modifier.fillMaxWidth().weight(1f), // Fill width for horizontal arrangement
                 horizontalArrangement = Arrangement.SpaceAround // Distribute buttons evenly
             ) {
                 CalculatorButton(text = "CE", modifier = Modifier.weight(1f), onClick = { buttonNum("CE") })
@@ -104,7 +122,7 @@ fun CalculatorScreen(modifier: Modifier = Modifier) {
                 CalculatorButton(text = "%", modifier = Modifier.weight(1f), onClick = { buttonNum("%") })
                 CalculatorButton(text = "÷", modifier = Modifier.weight(1f), onClick = { buttonNum("÷") })
             }
-            Row(modifier = Modifier.fillMaxWidth(),
+            Row(modifier = Modifier.fillMaxWidth().weight(1f),
                 horizontalArrangement = Arrangement.SpaceAround
             ) {
                 CalculatorButton(text = "7", modifier = Modifier.weight(1f), onClick = { buttonNum("7") })
@@ -112,7 +130,7 @@ fun CalculatorScreen(modifier: Modifier = Modifier) {
                 CalculatorButton(text = "9", modifier = Modifier.weight(1f), onClick = { buttonNum("9") })
                 CalculatorButton(text = "x", modifier = Modifier.weight(1f), onClick = { buttonNum("x") })
             }
-            Row(modifier = Modifier.fillMaxWidth(),
+            Row(modifier = Modifier.fillMaxWidth().weight(1f),
                 horizontalArrangement = Arrangement.SpaceAround
             ) {
                 CalculatorButton(text = "4", modifier = Modifier.weight(1f), onClick = { buttonNum("4") })
@@ -120,7 +138,7 @@ fun CalculatorScreen(modifier: Modifier = Modifier) {
                 CalculatorButton(text = "6", modifier = Modifier.weight(1f), onClick = { buttonNum("6") })
                 CalculatorButton(text = "-", modifier = Modifier.weight(1f), onClick = {buttonNum("-") })
             }
-            Row(modifier = Modifier.fillMaxWidth(),
+            Row(modifier = Modifier.fillMaxWidth().weight(1f),
                 horizontalArrangement = Arrangement.SpaceAround
             ) {
                 CalculatorButton(text = "1", modifier = Modifier.weight(1f), onClick = { buttonNum("1") })
@@ -129,17 +147,19 @@ fun CalculatorScreen(modifier: Modifier = Modifier) {
                 CalculatorButton(text = "+", modifier = Modifier.weight(1f), onClick = { buttonNum("+") })
             }
             Row(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier.fillMaxSize().weight(1f),
                 horizontalArrangement = Arrangement.SpaceAround
             ) {
                 Button(
+                    onClick = { buttonNum("0") },
+                    shape = RoundedCornerShape(16.dp),
                     modifier = Modifier
                         .weight(2f)
-                        .fillMaxHeight()
-                        .padding(horizontal = 4.dp),
-                    shape = RoundedCornerShape(16.dp),
-                    onClick = { buttonNum("0") }) {
-                    Text(text = "0", fontSize = 30.sp)
+                        .aspectRatio(2f)
+                        .padding(horizontal = 4.dp, vertical = 4.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF415BAA), contentColor = Color.White)
+                ) {
+                    Text(text = "0", fontSize = 25.sp)
                 }
                 CalculatorButton(text = ".", modifier = Modifier.weight(1f), onClick = { buttonNum(".") })
                 CalculatorButton(text = "=", modifier = Modifier.weight(1f), onClick = { buttonNum("=") })
@@ -154,28 +174,51 @@ fun CalculatorButton(
     text: String,
     onClick: () -> Unit
 ) {
+    val isOperator = text !in "123456789."
     Button(
         onClick = onClick,
         shape = RoundedCornerShape(16.dp),
         modifier = modifier
             .padding(4.dp)
-            .aspectRatio(1f)
+            .aspectRatio(1f),
+        colors = ButtonDefaults.buttonColors( // Set button colors
+            containerColor = if (isOperator) Color(0xFF3D4355) else Color(0xFF415BAA),
+            contentColor = Color.White
+        )
+
     ) {
-        Text(text = text, fontSize = 30.sp)
+        Text(text = text, fontSize = 25.sp)
     }
 }
 
 @Composable
-fun textBox(text: String, modifier: Modifier = Modifier) {
-    Text(
-        text = text,
-        fontSize = 150.sp,
-        textAlign = TextAlign.Right,
+fun textBox(text: String, textStyle: MutableState<TextStyle>, modifier: Modifier = Modifier) {
+    var check by remember { mutableStateOf(false) }
+    Box(
         modifier = modifier
             .fillMaxWidth()
-            .padding(30.dp)
-    )
+            .padding(10.dp)
+            .background(Color.Black)
+            .border(1.dp, Color.Gray)
+            .height(170.dp),
+        contentAlignment = Alignment.CenterEnd // Align to right and center vertically
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier.drawWithContent { if(check) drawContent() },
+            style = textStyle.value,
+            softWrap = false,
+            onTextLayout = { textLayoutResult ->
+                if (textLayoutResult.didOverflowWidth || textLayoutResult.didOverflowHeight) {
+                    check = false
+                    textStyle.value = textStyle.value.copy(fontSize = textStyle.value.fontSize * 0.9)
+                } else {
+                    check = true
+                }
+            }
+        )
 
+    }
 }
 
 
